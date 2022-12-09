7447e072fd61f72c50ab2a718dedb21b2ae70a55
Nathan <ndbrewer@mtu.edu>
2022-12-08 22:26:58 -0500

    commit

---
+++ src/Splat.java
@@ -0,0 +1,26 @@
+/**
+ * This is the Splat assignment.
+ * <Description here>
+ *
+ * @author Nathan Brewer
+ * <p>
+ * CS1131, 12/8/2022
+ * The SplatProject project.
+ */
+public class Splat {
+	
+	
+	/**
+	 * The main method is called auto-magically by Java to start the program.
+	 * <Description here>
+	 *
+	 * @param args Contains a list of command-line arguments.
+	 * @return Nothing.
+	 */
+	public static void main( String[] args ) {
+		// code goes here...
+		System.out.println( "Hello World!" );
+		
+	}
+	
+}
